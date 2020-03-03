adbdad359724040473c8e3e3b98326de2d482aeb
NattawatQew <61580501+NattawatQew@users.noreply.github.com>
2020-03-03 19:50:48 +0700

    Add gender page(finish)

--- app/src/main/java/com/eatburn/calorieday/GenderActivity.kt
+++ app/src/main/java/com/eatburn/calorieday/GenderActivity.kt
@@ -2,26 +2,30 @@ package com.eatburn.calorieday
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_gender.*
 
 class GenderActivity : AppCompatActivity() {
 
     var mAuth: FirebaseAuth? = null
     private lateinit var mDatabase: DatabaseReference
-    private val TAG: String = "Gender Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gender)
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+        val user = mAuth!!.currentUser
+        val uid = user!!.uid
         male_btn.setOnClickListener{
+            mDatabase.child(uid).child("UserInfo").child("Gender").setValue("Male")
             startActivity(Intent(this@GenderActivity, ResultActivity::class.java))
         }
         female_btn.setOnClickListener{
+            mDatabase.child(uid).child("UserInfo").child("Gender").setValue("Female")
             startActivity(Intent(this@GenderActivity, ResultActivity::class.java))
         }
     }
