a21ad60d49bd8c072bdc252f8cd77477aac53d69
NattawatQew <61580501+NattawatQew@users.noreply.github.com>
2020-03-04 13:24:31 +0700

    Add pages (No Database)

--- app/src/main/java/com/eatburn/calorieday/MyProgramActivity.kt
+++ app/src/main/java/com/eatburn/calorieday/MyProgramActivity.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_goal_weight.*
-import kotlinx.android.synthetic.main.activity_helth_info.*
+import kotlinx.android.synthetic.main.activity_my_program.*
 
-class GoalWeightActivity : AppCompatActivity() {
+class MyProgramActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_goal_weight)
-        goal_continueBtn.setOnClickListener {
-            startActivity(Intent(this@GoalWeightActivity, MyProgramActivity::class.java))
+        setContentView(R.layout.activity_my_program)
+        myprogram_continueBtn.setOnClickListener {
+            startActivity(Intent(this@MyProgramActivity, ReadyActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/eatburn/calorieday/ReadyActivity.kt
@@ -0,0 +1,18 @@
+package com.eatburn.calorieday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_program.*
+import kotlinx.android.synthetic.main.activity_ready.*
+
+class ReadyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ready)
+        ready_continueBtn.setOnClickListener {
+            startActivity(Intent(this@ReadyActivity, ProfileActivity::class.java))
+        }
+    }
+}
